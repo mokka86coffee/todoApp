0000e95a6828ba78b8d4332d4505e38aa09f3e7f
mokka86coffee <mokka86coffee@gmail.com>
2019-04-04 16:42:46 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import PropTypes from 'prop-types';
+
+import AddToDo from './containers/AddToDo';
+import ToDoListContainer from './containers/ToDoListContainer';
 
 class App extends Component {
   render() {
     return (
       <Container>
         <Row className="row">
-          <Col lg={12}>
-            <h1 className="mt-5">{this.props.title}</h1>
-            <Button>Look, I'm a button!</Button>
+          <Col xs={12}>
+            <h1>To Do List</h1>
+            <AddToDo />
+            <ToDoListContainer />
           </Col>
         </Row>
       </Container>
@@ -21,7 +23,3 @@ class App extends Component {
 }
 
 export default App;
-
-App.propTypes = {
-    title: PropTypes.string.isRequired
-}
